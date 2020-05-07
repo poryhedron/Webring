bac5d560ebcab4433fd5bdc6a0afe86558e9344c
poryhedron <55483958+poryhedron@users.noreply.github.com>
2020-05-06 23:02:14 -0500

    Update sites.js

--- scripts/sites.js
+++ scripts/sites.js
@@ -879,9 +879,16 @@ const sites = [
     contact: 'dl.amorphous@gmail.com',
     langs: ['en'],
     url: 'https://amorphic.space'
+   },
+  
+  {
+    author: 'b-liew',
+    contact: 'contact@b-liew.com',
+    langs: ['en'],
+    title: 'b-liew',
+    type: 'hybrid',
+    url: 'https://www.b-liew.com'
   }
-]
-
 /*
 Make sure you've read the README!
 Don't forget the comma on the previous entry!
